b502ef4fec68a7f76591ca9687d74cf4ed59b6a9
ItsArnob <itsarnob@users.noreply.github.com>
2021-10-24 00:29:19 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: "poppins, sans-serif",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
